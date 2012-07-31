216694f7d3dd1169b0b1ed595ed9d27475d6064e
hetang <hetang.shah@gmail.com>
2012-07-31 14:37:11 -0700

    JGraph Resrtuctured

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ JGraph
 ======
 
 Graph framework for mobile, tablet and Desktop application
+
+To Test Pie Graph:
+
+// testing
+var arr1= [20,34,30],
+    g = new JGraph(arr1, "board");
+	g.pie();
--- scripts/JGraph.js
+++ scripts/JGraph.js
@@ -1,60 +1,95 @@
-// rishi ghan
-// trying to create a general purpose graphing class 
-// july 26, 2012
+// Rishi Ghan & Hetang Shah
+// Trying to create a general purpose graphing class 
+// July 26, 2012
 
-function JGraph (xdata, divid){
-	var sum = 0,
-		percentages =[],
-		angles =[],
-		canvas = document.getElementById(divid);	
+function JGraph (pData, pCanvas, options){
+
+	if(!(this instanceof JGraph)) {
+		return new JGraph();
+	}
 	
-	// sums up the elements in an array
-	this.sum = function(arr) {
-		for(var i=0; i<arr.length; i++)
-		{
-			sum+= arr[i];
-		}
-		return sum;
+	this.graphData = pData || null;
+	this.canvasObj = null;
+	
+	if(arguments.length === 1) {
+		this.graphData = options['data'] || null;
+		pCanvas = options['canvas'];
+	}
+	
+	if (pCanvas instanceof HTMLElement){
+		this.canvasObj = pCanvas;
+	} else if (typeof pCanvas == 'string') {
+		this.canvasObj = document.getElementById(pCanvas);
+	} else {
+		this.canvasObj = null;
 	};
+}
+//end the class definition
+
+//sums up the elements in an array
+JGraph.prototype.sum = function(arr) {
+	var sum = 0;
+	for(var i=0; i<arr.length; i++) {
+		sum+= arr[i];
+	}
+	return sum;
+};
+
+// draws a pie chart when you supply an array and a canvas id
+JGraph.prototype.pie =  function(pData, pCanvas){
+	var canvasObj = this.canvasObj;
+	var graphData = pData || this.graphData;
 	
-	// draws a pie chart when you supply an array and a canvas id
-	this.pie =  function(){
-		var totaldatapoints = this.sum(xdata);
-		 
-		// loop through data points array and calculate percentages.
-		for (var i=0; i < xdata.length; i++) {
-			percentages[i] = Math.ceil((xdata[i]/totaldatapoints) *100); // percentages
-			angles[i] = Math.ceil((xdata[i]/totaldatapoints) *360)  //angles
-			console.log(angles[i]);
+	if(pCanvas) {
+		if (pCanvas instanceof HTMLElement){
+			canvasObj = pCanvas;
+		} else if (typeof pCanvas == 'string') {
+			canvasObj = document.getElementById(pCanvas);
 		};
-		
-		// find the fucking canvas element
-		context = canvas.getContext("2d");
-		
-		// co-ordinates (where should they be initialized ?) 
-		var x = canvas.width / 2,
-		  y = canvas.height / 2,
-		  startAngle = 0,
-		  endAngle = 0,
-		  radius = 100, // this should be customizable.
-		  counterclockwise = true;
-		
-		// loop through the angles array and draw arcs, bitch.
-	    // also show the edges of the pie slices ?
+	}	
+	
+	if(!canvasObj) {
+		throw {
+			name: 'JGraph Error',
+			message: 'No Canvas Object defined'			
+		};
+	}
+	
+	if(!graphData) {
+		throw {
+			name: 'JGraph Error',
+			message: 'No Graph points given'			
+		};
+	}
+	var totaldatapoints = this.sum(graphData);
 	 
-
-		//draw a line back to the arc
-		for(var j=1; j<=angles.length;j++){
-			context.beginPath(); 
-			context.arc(x,y,radius,angles[j-1], angles[0], counterclockwise);
-			context.lineTo(x,y);
-			//context.fillText(percentages[i]);
-			context.stroke();  
-	    };
-		context.closePath();
+	// loop through data points array and calculate percentages.
+	for (var i=0; i < graphData.length; i++) {
+		percentages[i] = Math.ceil((graphData[i]/totaldatapoints) *100); // percentages
+		angles[i] = Math.ceil((graphData[i]/totaldatapoints) *360);  //angles
+		console.log(angles[i]);
 	};
 	
-	this.ydatapoints =  function(){
-		console.log(ydata);
-	};
-} //end the class definition
+	// find the fucking canvas element
+	context = canvasObj.getContext("2d");
+	
+	// co-ordinates (where should they be initialized ?) 
+	var x = canvas.width / 2,
+	  y = canvas.height / 2,
+	  //startAngle = 0,
+	  //endAngle = 0,
+	  radius = 100, // this should be customizable.
+	  counterclockwise = true;
+	
+	/*	loop through the angles array and draw arcs, bitch.
+    	also show the edges of the pie slices ?
+		draw a line back to the arc */
+	for(var j=1; j<=angles.length;j++){
+		context.beginPath(); 
+		context.arc(x,y,radius,angles[j-1], angles[0], counterclockwise);
+		context.lineTo(x,y);
+		//context.fillText(percentages[i]);
+		context.stroke();  
+    };
+	context.closePath();
+};	
